02dc383e20728ee85327824f3cc29efb2c24290a
Andrew Hahn <dev.andrewhahn@gmail.com>
2024-10-16 11:36:34 +0900

    Changed `get_key_name` return type and updated syntax for digest methods
    Signed-off-by: Andrew Hahn <dev.andrewhahn@gmail.com>

--- src/digest.rs
+++ src/digest.rs
@@ -1,4 +1,5 @@
-use crate::raw;
+use crate::{raw, ValkeyString};
+
 use std::os::raw::c_char;
 
 /// `Digest` is a high-level rust interface to the Valkey module C API
@@ -17,56 +18,51 @@ impl Digest {
     /// # Panics
     ///
     /// Will panic if `RedisModule_GetKeyNameFromDigest` is missing in redismodule.h
-    pub fn get_key_name(&self) -> *const raw::RedisModuleString {
-        unsafe {
-            match raw::RedisModule_GetKeyNameFromDigest {
-                Some(get_key_name_from_digest) => get_key_name_from_digest(self.dig),
-                None => panic!("RedisModule_GetKeyNameFromDigest is not available."),
-            }
-        }
+    pub fn get_key_name(&self) -> ValkeyString {
+        ValkeyString::from_redis_module_string(std::ptr::null_mut(), unsafe {
+            raw::RedisModule_GetKeyNameFromDigest
+                .expect("RedisModule_GetKeyNameFromDigest is not available.")(self.dig)
+            .cast_mut()
+        })
     }
 
-    /// Returns the database id of this [`Digest`].
+    /// Returns the database ID of this [`Digest`].
     ///
     /// # Panics
     ///
     /// Will panic if `RedisModule_GetDbIdFromDigest` is missing in redismodule.h
     pub fn get_db_id(&self) -> i32 {
         unsafe {
-            match raw::RedisModule_GetDbIdFromDigest {
-                Some(get_db_id_from_digest) => get_db_id_from_digest(self.dig),
-                None => panic!("RedisModule_GetDbIdFromDigest is not available."),
-            }
+            raw::RedisModule_GetDbIdFromDigest
+                .expect("RedisModule_GetDbIdFromDigest is not available.")(self.dig)
         }
     }
 
-    /// Converts the long long input to a string and adds it to this [`Digest`].
-    ///
-    /// # Panics
-    ///
-    /// Will panic if `RedisModule_DigestAddLongLong` is missing in redismodule.h
-    pub fn add_long_long(&mut self, ll: i64) {
-        unsafe {
-            match raw::RedisModule_DigestAddLongLong {
-                Some(digest_add_long_long) => digest_add_long_long(self.dig, ll),
-                None => panic!("RedisModule_DigestAddLongLong is not available."),
-            }
-        }
-    }
-
-    /// Add a new element to this [`Digest`].
+    /// Adds a new element to this [`Digest`].
     ///
     /// # Panics
     ///
     /// Will panic if `RedisModule_DigestAddStringBuffer` is missing in redismodule.h
     pub fn add_string_buffer(&mut self, ele: &[u8]) {
         unsafe {
-            match raw::RedisModule_DigestAddStringBuffer {
-                Some(digest_add_string_buffer) => {
-                    digest_add_string_buffer(self.dig, ele.as_ptr().cast::<c_char>(), ele.len())
-                }
-                None => panic!("RedisModule_DigestAddStringBuffer is not available."),
-            }
+            raw::RedisModule_DigestAddStringBuffer
+                .expect("RedisModule_DigestAddStringBuffer is not available.")(
+                self.dig,
+                ele.as_ptr().cast::<c_char>(),
+                ele.len(),
+            )
+        }
+    }
+
+    /// Similar to [`Digest::add_string_buffer`], but takes [`i64`].
+    ///
+    /// # Panics
+    ///
+    /// Will panic if `RedisModule_DigestAddLongLong` is missing in redismodule.h
+    pub fn add_long_long(&mut self, ll: i64) {
+        unsafe {
+            raw::RedisModule_DigestAddLongLong
+                .expect("RedisModule_DigestAddLongLong is not available.")(self.dig, ll)
         }
     }
 
@@ -77,10 +73,8 @@ impl Digest {
     /// Will panic if `RedisModule_DigestEndSequence` is missing in redismodule.h
     pub fn end_sequence(&mut self) {
         unsafe {
-            match raw::RedisModule_DigestEndSequence {
-                Some(digest_end_sequence) => digest_end_sequence(self.dig),
-                None => panic!("RedisModule_DigestEndSequence is not available."),
-            }
+            raw::RedisModule_DigestEndSequence
+                .expect("RedisModule_DigestEndSequence is not available.")(self.dig)
         }
     }
 }
