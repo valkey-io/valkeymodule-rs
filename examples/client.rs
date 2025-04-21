6e80e85a60c4b00ce1c686a87147bf101a10b6f2
Dmitry Polyakovsky <dmitrypol@users.noreply.github.com>
2025-04-21 12:10:01 -0700

    New functions in Context client (#193)
    * updated version to 0.1.8
    
    Signed-off-by: Dmitry Polyakovsky <dmitry.polyakovky@oracle.com>
    
    * adding new functions to Context client to get by ID
    added get_client_ip
    return Status from set_client_name
    updated tests and example
    
    Signed-off-by: Dmitry Polyakovsky <dmitry.polyakovky@oracle.com>
    
    * return ValkeyResult<ValkeyString> from *by_id functions
    
    Signed-off-by: Dmitry Polyakovsky <dmitry.polyakovky@oracle.com>
    
    * use match instead of unwrap in context/client.rs to handle when underlying Redis/Valkey does not have the function and not crash the process
    
    Signed-off-by: dmitrypol <dmitrypol@gmail.com>
    Signed-off-by: Dmitry Polyakovsky <dmitry.polyakovky@oracle.com>
    
    * reverted to use unwrap
    
    Signed-off-by: dmitrypol <dmitrypol@gmail.com>
    Signed-off-by: Dmitry Polyakovsky <dmitry.polyakovky@oracle.com>
    
    * updated integ tests
    
    Signed-off-by: dmitrypol <dmitrypol@gmail.com>
    Signed-off-by: Dmitry Polyakovsky <dmitry.polyakovky@oracle.com>
    
    * updated responses to use ValkeyResult in more places
    better error messages
    
    Signed-off-by: dmitrypol <dmitrypol@gmail.com>
    Signed-off-by: Dmitry Polyakovsky <dmitry.polyakovky@oracle.com>
    
    ---------
    
    Signed-off-by: Dmitry Polyakovsky <dmitry.polyakovky@oracle.com>
    Signed-off-by: dmitrypol <dmitrypol@gmail.com>
    Co-authored-by: Dmitry Polyakovsky <dmitry.polyakovky@oracle.com>

--- examples/client.rs
+++ examples/client.rs
@@ -1,33 +1,37 @@
 use valkey_module::alloc::ValkeyAlloc;
-use valkey_module::{valkey_module, Context, NextArg, ValkeyError, ValkeyResult, ValkeyString};
-
-valkey_module! {
-    name: "client",
-    version: 1,
-    allocator: (ValkeyAlloc, ValkeyAlloc),
-    data_types: [],
-    commands: [
-        ["client.id", get_client_id, "readonly", 0, 0, 0],
-        ["client.name", set_client_name, "readonly", 0, 0, 0],
-    ]
-}
+use valkey_module::{
+    valkey_module, Context, NextArg, ValkeyError, ValkeyResult, ValkeyString, ValkeyValue,
+};
 
 fn get_client_id(ctx: &Context, _args: Vec<ValkeyString>) -> ValkeyResult {
     let client_id = ctx.get_client_id();
-    ctx.log_notice(&format!(
-        "client_username: {:?}",
-        ctx.get_client_username().to_string()
-    ));
-    ctx.log_notice(&format!(
-        "client_name: {:?}",
-        ctx.get_client_name().to_string()
-    ));
-    ctx.log_notice(&format!(
-        "client_cert: {:?}",
-        ctx.get_client_cert().to_string()
-    ));
-    ctx.log_notice(&format!("client_info: {:?}", ctx.get_client_info()));
     Ok((client_id as i64).into())
+}
+
+fn get_client_name(ctx: &Context, _args: Vec<ValkeyString>) -> ValkeyResult {
+    // test for invalid client_id
+    match ctx.get_client_name_by_id(0) {
+        Ok(tmp) => ctx.log_notice(&format!(
+            "client_id 0 client_name_by_id: {:?}",
+            tmp.to_string()
+        )),
+        Err(err) => ctx.log_notice(&format!("client_id 0 client_name_by_id: {:?}", err)),
+    }
+    let client_name = ctx.get_client_name()?;
+    Ok(ValkeyValue::from(client_name.to_string()))
+}
+
+fn get_client_username(ctx: &Context, _args: Vec<ValkeyString>) -> ValkeyResult {
+    // test for invalid client_id
+    match ctx.get_client_username_by_id(0) {
+        Ok(tmp) => ctx.log_notice(&format!(
+            "client_id 0 client_username_by_id: {:?}",
+            tmp.to_string()
+        )),
+        Err(err) => ctx.log_notice(&format!("client_id 0 client_username_by_id: {:?}", err)),
+    }
+    let client_username = ctx.get_client_username()?;
+    Ok(ValkeyValue::from(client_username.to_string()))
 }
 
 fn set_client_name(ctx: &Context, args: Vec<ValkeyString>) -> ValkeyResult {
@@ -36,6 +40,54 @@ fn set_client_name(ctx: &Context, args: Vec<ValkeyString>) -> ValkeyResult {
     }
     let mut args = args.into_iter().skip(1);
     let client_name = args.next_arg()?;
-    let resp = ctx.set_client_name(&client_name);
-    Ok(resp.into())
+    // test for invalid client_id
+    let resp1 = ctx.set_client_name_by_id(0, &client_name);
+    ctx.log_notice(&format!("client_id 0 set_client_name_by_id: {:?}", resp1));
+    let resp2 = ctx.set_client_name(&client_name);
+    Ok(ValkeyValue::Integer(resp2 as i64))
+}
+
+fn get_client_cert(ctx: &Context, _args: Vec<ValkeyString>) -> ValkeyResult {
+    // unless connection is made with cert, this will return Err, so just log it and return nothing
+    match ctx.get_client_cert() {
+        Ok(tmp) => ctx.log_notice(&format!("client_cert: {:?}", tmp.to_string())),
+        Err(err) => ctx.log_notice(&format!("client_cert: {:?}", err.to_string())),
+    }
+    Ok("".into())
+}
+
+fn get_client_info(ctx: &Context, _args: Vec<ValkeyString>) -> ValkeyResult {
+    // test for invalid client_id
+    let client_info_by_id = ctx.get_client_info_by_id(0);
+    ctx.log_notice(&format!(
+        "client_id 0 client_info_by_id: {:?}",
+        client_info_by_id
+    ));
+    let client_info = ctx.get_client_info()?;
+    ctx.log_notice(&format!("client_info: {:?}", client_info));
+    // return version like this:
+    Ok(ValkeyValue::from(client_info.version.to_string()))
+}
+
+fn get_client_ip(ctx: &Context, _args: Vec<ValkeyString>) -> ValkeyResult {
+    // test for invalid client_id
+    let client_ip_by_id = ctx.get_client_ip_by_id(0);
+    ctx.log_notice(&format!("client_ip_by_id: {:?}", client_ip_by_id));
+    Ok(ctx.get_client_ip().into())
+}
+
+valkey_module! {
+    name: "client",
+    version: 1,
+    allocator: (ValkeyAlloc, ValkeyAlloc),
+    data_types: [],
+    commands: [
+        ["client.id", get_client_id, "", 0, 0, 0],
+        ["client.get_name", get_client_name, "", 0, 0, 0],
+        ["client.set_name", set_client_name, "", 0, 0, 0],
+        ["client.username", get_client_username, "", 0, 0, 0],
+        ["client.cert", get_client_cert, "", 0, 0, 0],
+        ["client.info", get_client_info, "", 0, 0, 0],
+        ["client.ip", get_client_ip, "", 0, 0, 0],
+    ]
 }
--- src/context/client.rs
+++ src/context/client.rs
@@ -1,8 +1,10 @@
 use crate::{
     Context, RedisModuleClientInfo, RedisModule_GetClientCertificate, RedisModule_GetClientId,
     RedisModule_GetClientInfoById, RedisModule_GetClientNameById,
-    RedisModule_GetClientUserNameById, RedisModule_SetClientNameById, ValkeyString,
+    RedisModule_GetClientUserNameById, RedisModule_SetClientNameById, Status, ValkeyError,
+    ValkeyResult, ValkeyString,
 };
+use std::ffi::CStr;
 use std::os::raw::c_void;
 
 impl RedisModuleClientInfo {
@@ -19,48 +21,106 @@ impl RedisModuleClientInfo {
 }
 
 /// GetClientNameById, GetClientUserNameById and GetClientCertificate use autoMemoryAdd on the ValkeyModuleString pointer
-// after the callback (command, server event handler, ...) these ValkeyModuleString pointers will be freed automatically
+/// after the callback (command, server event handler, ...) these ValkeyModuleString pointers will be freed automatically
 impl Context {
     pub fn get_client_id(&self) -> u64 {
         unsafe { RedisModule_GetClientId.unwrap()(self.ctx) }
     }
 
-    pub fn get_client_name(&self) -> ValkeyString {
-        let client_id = self.get_client_id();
+    /// wrapper for RedisModule_GetClientNameById
+    pub fn get_client_name_by_id(&self, client_id: u64) -> ValkeyResult<ValkeyString> {
         let client_name = unsafe { RedisModule_GetClientNameById.unwrap()(self.ctx, client_id) };
-        ValkeyString::from_redis_module_string(self.ctx, client_name)
+        if client_name.is_null() {
+            Err(ValkeyError::Str("Client/Client name is null"))
+        } else {
+            Ok(ValkeyString::from_redis_module_string(
+                self.ctx,
+                client_name,
+            ))
+        }
     }
 
-    pub fn set_client_name(&self, client_name: &ValkeyString) -> i64 {
-        let client_id = self.get_client_id();
+    /// wrapper for RedisModule_GetClientNameById using current client ID
+    pub fn get_client_name(&self) -> ValkeyResult<ValkeyString> {
+        self.get_client_name_by_id(self.get_client_id())
+    }
+
+    /// wrapper for RedisModule_SetClientNameById
+    pub fn set_client_name_by_id(&self, client_id: u64, client_name: &ValkeyString) -> Status {
         let resp = unsafe { RedisModule_SetClientNameById.unwrap()(client_id, client_name.inner) };
-        resp as i64
+        Status::from(resp)
     }
 
-    pub fn get_client_username(&self) -> ValkeyString {
-        let client_id = self.get_client_id();
+    /// wrapper for RedisModule_SetClientNameById using current client ID
+    pub fn set_client_name(&self, client_name: &ValkeyString) -> Status {
+        self.set_client_name_by_id(self.get_client_id(), client_name)
+    }
+
+    /// wrapper for RedisModule_GetClientUserNameById
+    pub fn get_client_username_by_id(&self, client_id: u64) -> ValkeyResult<ValkeyString> {
         let client_username =
             unsafe { RedisModule_GetClientUserNameById.unwrap()(self.ctx, client_id) };
-        ValkeyString::from_redis_module_string(self.ctx, client_username)
+        if client_username.is_null() {
+            Err(ValkeyError::Str("Client/Username is null"))
+        } else {
+            Ok(ValkeyString::from_redis_module_string(
+                self.ctx,
+                client_username,
+            ))
+        }
     }
 
-    pub fn get_client_username_by_id(&self, client_id: u64) -> ValkeyString {
-        let client_username =
-            unsafe { RedisModule_GetClientUserNameById.unwrap()(self.ctx, client_id) };
-        ValkeyString::from_redis_module_string(self.ctx, client_username)
+    /// wrapper for RedisModule_GetClientUserNameById using current client ID
+    pub fn get_client_username(&self) -> ValkeyResult<ValkeyString> {
+        self.get_client_username_by_id(self.get_client_id())
     }
 
-    pub fn get_client_cert(&self) -> ValkeyString {
+    /// wrapper for RedisModule_GetClientCertificate
+    pub fn get_client_cert(&self) -> ValkeyResult<ValkeyString> {
         let client_id = self.get_client_id();
         let client_cert = unsafe { RedisModule_GetClientCertificate.unwrap()(self.ctx, client_id) };
-        ValkeyString::from_redis_module_string(self.ctx, client_cert)
+        if client_cert.is_null() {
+            Err(ValkeyError::Str("Client/Cert is null"))
+        } else {
+            Ok(ValkeyString::from_redis_module_string(
+                self.ctx,
+                client_cert,
+            ))
+        }
     }
 
-    pub fn get_client_info(&self) -> RedisModuleClientInfo {
-        let client_id = self.get_client_id();
+    /// wrapper for RedisModule_GetClientInfoById
+    pub fn get_client_info_by_id(&self, client_id: u64) -> ValkeyResult<RedisModuleClientInfo> {
         let mut mci = RedisModuleClientInfo::new();
         let mci_ptr: *mut c_void = &mut mci as *mut _ as *mut c_void;
-        unsafe { RedisModule_GetClientInfoById.unwrap()(mci_ptr, client_id) };
-        mci
+        unsafe {
+            RedisModule_GetClientInfoById.unwrap()(mci_ptr, client_id);
+        };
+        if mci_ptr.is_null() {
+            Err(ValkeyError::Str("Client/Info is null"))
+        } else {
+            Ok(mci)
+        }
+    }
+
+    /// wrapper for RedisModule_GetClientInfoById using current client ID
+    pub fn get_client_info(&self) -> ValkeyResult<RedisModuleClientInfo> {
+        self.get_client_info_by_id(self.get_client_id())
+    }
+
+    /// wrapper to get the client IP address from RedisModuleClientInfo
+    pub fn get_client_ip_by_id(&self, client_id: u64) -> ValkeyResult<String> {
+        let client_info = self.get_client_info_by_id(client_id)?;
+        let c_str_addr = unsafe { CStr::from_ptr(client_info.addr.as_ptr()) };
+        let ip_addr_as_string = c_str_addr.to_string_lossy().into_owned();
+        Ok(ip_addr_as_string)
+    }
+
+    /// wrapper to get the client IP address from RedisModuleClientInfo using current client ID
+    pub fn get_client_ip(&self) -> String {
+        match self.get_client_ip_by_id(self.get_client_id()) {
+            Ok(tmp) => tmp,
+            Err(_err) => "Client IP is null".to_string(),
+        }
     }
 }
