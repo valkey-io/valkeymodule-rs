25c45b47a587a7971c8093fe74cf9d47efb5bbcd
martinrvisser <martinrvisser@users.noreply.github.com>
2026-09-24 15:38:31 +0100

    Adding HANDLE_ATOMIC_SLOT_MIGRATION (#246)
    * Adding HANDLE_ATOMIC_SLOT_MIGRATION
    
    Signed-off-by: martinrvisser <mvisser@hotmail.com>
    
    * Adding VALKEYMODULE_OPTIONS_SKIP_COMMAND_VALIDATION
    
    Signed-off-by: martinrvisser <mvisser@hotmail.com>
    
    ---------
    
    Signed-off-by: martinrvisser <mvisser@hotmail.com>

--- valkeymodule-rs-macros-internals/src/api_versions.rs
+++ valkeymodule-rs-macros-internals/src/api_versions.rs
@@ -31,6 +31,7 @@ lazy_static::lazy_static! {
         (70000, "min-redis-compatibility-version-7-0".to_string()),
         (70200, "min-redis-compatibility-version-7-2".to_string()),
         (80000, "min-valkey-compatibility-version-8-0".to_string()),
+        (90000, "min-valkey-compatibility-version-9-0".to_string()),
     ];
 }
 
